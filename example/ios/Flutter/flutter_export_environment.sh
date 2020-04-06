a7cb16806f8a162a08455f511a043c234bcfcb2c
Tornike Kurdadze <qurdadzze@gmail.com>
2020-04-06 13:11:06 +0400

    Migrated to AndroidX

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/dev/Dev/Tools/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/dev/Dev/Projects/Flutter/Flutter_PinPut/example"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/dev/Dev/Tools/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
