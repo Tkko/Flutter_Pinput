109cfff67f669e1ed59105aae4445426ca8780d4
Adrian Flutur <ady_flutur@yahoo.com>
2021-08-03 01:16:02 +0300

    Fixed example app pin overflow on first pin input

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -132,6 +132,8 @@ class PinPutViewState extends State<PinPutView> {
                 return RoundedButton(
                   title: '$e',
                   onTap: () {
+                    if (_pinPutController.text.length >= 5) return;
+
                     _pinPutController.text = '${_pinPutController.text}$e';
                   },
                 );
