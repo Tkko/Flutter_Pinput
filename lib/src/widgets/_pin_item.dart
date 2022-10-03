1d00d249ecf27005c2a31067f93befba35cb1f89
Tkko <qurdadzze@gmail.com>
2022-10-03 20:44:23 +0400

    Clean up

--- lib/src/widgets/_pin_item.dart
+++ lib/src/widgets/_pin_item.dart
@@ -81,8 +81,9 @@ class _PinItem extends StatelessWidget {
 
     final isActiveField = index == pin.length;
     final focused = state.effectiveFocusNode.hasFocus || !state.widget.useNativeKeyboard;
+    final shouldShowCursor = state.widget.showCursor && state.isEnabled && isActiveField && focused;
 
-    if (state.widget.showCursor && state.isEnabled && isActiveField && focused) {
+    if (shouldShowCursor) {
       return _buildCursor(pinTheme);
     }
 
